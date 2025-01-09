38f85d10b083df9a212e6d0702afed1efd09cdf1
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-01-09 13:26:05 +0530

    82challenge

--- 82_Challenge_Question.js
+++ 82_Challenge_Question.js
@@ -1,72 +1,62 @@
-// 📌 **Hey Everyone 👋🏻 Welcome Back to Code with Poonam!**
-// It day 82 of our 100 Days of Coding Challenge in JavaScript!
-// 🎯 Day 82: Remove Duplicate Values from an Object in JavaScript**
-
-// 📝 **Problem Statement:**  
 // Write a JavaScript function to remove duplicate values from an object.  
 
 // 📦 **Input Example:**  
 // `obj = { a: 1, b: 2, c: 1, d: 3, e: 2 }`  
 // ✅ Output: `{ a: 1, b: 2, d: 3 }`  
 
-// 🛠️ **Approach:**  
-// To solve this, we will be using a Set Object*  
-// - A `Set` is a special JavaScript object that stores **unique values only**.  
-// - It automatically **removes duplicates**, which makes it perfect for this problem.  
-// - Also JavaScript objects only ensure unique keys, not unique values. So, an extra object (or Set) is needed to keep track of values
+// JavaScript function       >>removeDuplicates()
+// remove duplicate values from an object.
 
-// 1️⃣ **Create a Set:** Use a `Set` to keep track of unique values. 
-// uniqueValues = new Set{}   >>Store unique values
-// result = {}                >>Result object
+// Approach:
+// Set Object
+// Create a Set
+// uniqueValues=new Set() >>Track of unique values
+// result={} >>Store our Final Output
 
-// 2️⃣ **Loop through the object:** Use `Object.entries()` to extract key-value pairs.  
-//   for (const [key, value] of Object.entries(obj)) {
-// 3️⃣ **Check for Duplicates:** If the value is not already present in the `Set` (using `.has()`), add it and keep the key-value pair. 
-    // Logic to check if value is already in the set
-    // if its unqiue then add it to the set
-    // add key-value pair to result
+// Loop >>Object.entries() >>key value
+// obj ={ a: 1, b: 2, c: 1, d: 3, e: 2 }
+// Object.entires(obj)
+// [["a",1],["b,2"],["c",1],["d",3],["e",2]]
+// Destructing
+// const [key,value]=["a",1] >> key =a value =1
+
+// for(const [key,value] of Object.entries(obj)){
+    //remove duplicate values
+    // Chcek >value is present or not
+    // unique >>Add it to set
+    // Add to result {}
 // }
 
-// ✅ **Key Methods Used in Set:**  
-// - `.add(value)` → Adds a value to the set. If the value already exists, it won't be added again.  
-// - `.has(value)` → Checks if the value already exists in the set and returns `true` or `false`. 
-// if (!uniqueValues.has(value)) { // Step 4: Check if value is already in the set
-//     uniqueValues.add(value);  // Step 5: Add value to the set if unique
-//     result[key] = value;      // Step 6: Add key-value pair to result
+// Print our result {} >> Output
+
+// if condition
+// Set Methods
+// add(value) >>Add a value to the Set >>If exists then not added
+// has(value) >>Boolean Value >> True || False  >>Check that value is present or not
+
+// present >>TRue >> Existing Dont add >> No execute
+// -- !
+// --!true >>false >> if condition wont execute
+// --!false >>true >> if condition execute
+
+// if(!uniqueValues.has(value)){
+    // uniqueValues.add(value)
+    // result[key]=value
 // }
- 
-// 4️⃣ console.log(result) >>Print the filtered object with no duplicates.  
 
-// 🚀 **JavaScript Implementation:**
-function removeDuplicates(obj) {
-    const uniqueValues = new Set(); // Step 1: Store unique values
-    const result = {};  // Step 2: Result object
+function removeDuplicates(obj){
+   // console.log(obj)
 
-    // Step 3: Iterate through object entries
-    for (const [key, value] of Object.entries(obj)) {
-        if (!uniqueValues.has(value)) { // Step 4: Check if value is already in the set
-            uniqueValues.add(value);  // Step 5: Add value to the set if unique
-            result[key] = value;      // Step 6: Add key-value pair to result
+    let uniqueValues=new Set()
+    let result={}
+
+    for(const [key,value] of Object.entries(obj)){
+        if(!uniqueValues.has(value)){
+            uniqueValues.add(value)
+            result[key]=value
         }
     }
-
-    console.log(result) // Step 7: Return filtered object
+    console.log(result)
 }
-removeDuplicates({ a: 1, b: 2, c: 1, d: 3, e: 2 }); //{ a: 1, b: 2, d: 3 }
+removeDuplicates({ a: 1, b: 2, c: 1, d: 3, e: 2 })
 
-// 💡 **Explanation:**
-// - The value `1` appears twice, so only the first occurrence (`a`) is kept.  
-// - The value `2` appears twice, so only the first occurrence (`b`) is kept.  
-// - The value `3` is unique, so it remains in the result.  
-
-// 🎯 **Key Concepts Used:**  
-// - `Set`: To ensure only unique values are stored.  
-// - `.add()` and `.has()` methods of Set.  
-// - `Object.entries()` for iterating key-value pairs.  
-
-// ✅ **Why use a Set instead of an Array?**  
-// - A `Set` is optimized for **unique value storage** and **faster lookups** compared to an array.  
-// - Using an array would require extra work to filter duplicates manually.  
-
-// 🎉 **Don't forget to Like, Share, and Subscribe for more coding challenges!**  
-// 🚀 **Stay consistent and keep improving! See you tomorrow with another fun challenge!**  
--- test.js
+++ test.js
@@ -1,28 +1,279 @@
-const userInput = [
-    '2',
-    '7',
-    '2 4 6 8 9 10 12',
-    '2 4 6 8 10 12',
-    '6',
-    '3 5 7 9 11 13',
-    '3 5 7 11 13'
-];
+import { useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { url } from '../../utils/constant';
+import { Col } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
 
-const results = [];
-let index = 0;
-const testCases = parseInt(userInput[index++]);
 
-for (let t = 0; t < testCases; t++) {
-    const N = parseInt(userInput[index++]);
-    const batchA = userInput[index++].split(' ').map(Number);
-    const batchB = userInput[index++].split(' ').map(Number);
+const ModalAddAdmission = ({ show, setShow, setAdmissionData }) => {
+    const navigate = useNavigate()
+    const handleClose = () => {
+        setShow(false)
+        navigate('/admissiondata')
+    }
+    const [courseData, setCourseData] = useState([])
+    // const [courseName, setCourseName] = useState('');
 
-    for (let i = 0; i < batchA.length; i++) {
-        if (!batchB.includes(batchA[i])) {
-            results.push(i + 1); // Return the package index (1-based)
-            break;
+    //Original Course Data
+    const getCourseData = async () => {
+        console.log("Console data is called....")
+        let res = await axios.get(`${url}/allcourse`, config)
+        console.log("Course Data", res.data.courseData)
+        setCourseData(res.data.courseData)
+    }
+    useEffect(() => {
+        getCourseData()
+    }, [])
+    console.log(courseData)
+
+
+    const formSchema = Yup.object().shape({
+        courseName: Yup.string().required(),
+        admissionSource: Yup.string().required(),
+        admissionFee: Yup.number().required(),
+        admissionDate: Yup.date().required(),
+        admissionYear: Yup.number().required(),
+        admissionMonth: Yup.number().required(),
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            courseName: "",
+            admissionSource: "",
+            admissionFee: "",
+            admissionDate: "",
+            admissionYear: 2020,
+            admissionMonth: "Jan",
+            courseId: "",
+        },
+        // validationSchema:formSchema,
+        enableReinitialize: true,
+        onSubmit: (values) => {
+            console.log(values)
+            addAdmission(values)
+        }
+    })
+
+    const token = sessionStorage.getItem('token')
+    console.log(token)
+
+    let config = {
+        headers: {
+            Authorization: `Bearer ${token}`
         }
     }
+
+    const formatDate = (dateString) => {
+        console.log(dateString)
+        const date = new Date(dateString);
+        console.log(date)
+        return date.toLocaleDateString('en-US', {
+            year: "numeric",
+            month: 'long',
+            day: 'numeric'
+        })
+    }
+    console.log(new Date("03-01-2025"))
+
+    const dateFun = () => {
+        const date = new Date('Sat Mar 01 2025 00:00:00 GMT+0900 (Japan Standard Time)');
+        // Get the full month name
+        const month = date.toLocaleString('default', { month: 'long' });
+        // Get the full year
+        const year = date.getFullYear();
+        console.log(`${month} ${year}`);
+        return [month, year]
+    }
+
+
+    const addAdmission = async (newAdmission) => {
+        console.log(newAdmission)
+        const admission = {
+            ...newAdmission,
+
+            // Shoudld select from a dropdown
+            studentId: "674f9d1a62c9d9c5ca9df624",
+            // courseId: "677a1998ed75982c18d258fb"
+            courseId:newAdmission.courseId
+        }
+        try {
+            const res = await axios.post(`${url}/addadmission`, admission, config)
+            console.log(res)
+            if (res) {
+                let res = await axios.get(`${url}/alladmission`, config)
+                console.log("Successfully a new admission added to the DB!", admission)
+                setAdmissionData(res.data.admissionData)
+                // handleClose()
+            }
+        } catch (e) {
+            console.error("Error adding Admission:", e)
+        }
+    }
+    const handleCourseIdChange = (e) => {
+        const selectedCourseId = e.target.value;
+        const selectedCourse = courseData.find(course => course._id === selectedCourseId);
+        
+        // Update Formik state for courseId and courseName
+        formik.setFieldValue("courseId", selectedCourseId);
+        formik.setFieldValue("courseName", selectedCourse ? selectedCourse.courseName : "");
+      };
+    
+    
+    
+    return (
+        <Modal show={show}
+            onHide={handleClose}
+            size="lg" >
+            <Modal.Header>
+                <Modal.Title style={{ padding: "0% 5%" }}>Add Admission</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={formik.handleSubmit} style={{ padding: "1.5% 5%" }}>
+                <Modal.Body>
+                    <Row>
+                    <Col>
+                            {/* Course ID */}
+                                <Form.Group className='mt-3'>
+                                    <Form.Label className='mb-0'>Course ID</Form.Label>
+                                    <select
+                                    name="courseId"
+                                    className="form-select"
+                                    value={formik.values.courseId}
+                                    onChange={handleCourseIdChange}
+                                    >
+                                    <option value="">Select Course ID</option>
+                                    {courseData?.map((course) => (
+                                        <option key={course._id} value={course._id}>
+                                        {course._id}
+                                        </option>
+                                    ))}
+                                    </select>
+                                </Form.Group>
+
+                        </Col>
+                        
+                        <Col>
+                            
+
+                        <Form.Group className='mt-3'>
+                            <Form.Label className='mb-0'>Course Name</Form.Label>
+                            <input
+                            type="text"
+                            name="courseName"
+                            value={formik.values.courseName}  // Bind to Formik's courseName field
+                            readOnly  // Make it read-only since it auto-updates
+                            />
+                        </Form.Group>
+
+                        </Col>
+                    </Row>
+                    <Row>
+
+                        {/* Student ID */}
+                        <Col>
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Student ID</Form.Label>
+                                <select name="studentId" id="" className="form-select" value={formik.values.studentId} onChange={formik.handleChange}>
+                                    {/* {studentData?.map((element)=>
+                         <option key={element.studentId} value={element.studentId} >{element.studentId}</option>
+                        )}                        */}
+                                </select>
+                            </Form.Group>
+                        </Col>
+
+                        {/* Student Name */}
+                        <Col>
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Student Name</Form.Label>
+                                <select name="studentName" id="" className="form-select" value={formik.values.studentName} onChange={formik.handleChange}>
+                                    {/* {studentData?.map((element)=>
+                         <option key={element.studentName} value={element.studentName} >{element.studentName}</option>
+                        )}                        */}
+                                </select>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        {/* Admission Date */}
+                        <Col>
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Date</Form.Label>
+                                <Form.Control
+                                    type="date" placeholder='Type your Admission Date'
+                                    name='admissionDate' value={formik.values.admissionDate} onChange={formik.handleChange
+                                    } />
+                            </Form.Group>
+                        </Col>
+
+                        {/* Admission Month */}
+                        <Col>
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Month</Form.Label>
+                                <Form.Control disabled
+                                    type="text" placeholder='Type your Admission Month'
+                                    name="admissionMonth" value={formik.values.admissionMonth} onChange={formik.handleChange} />
+                            </Form.Group>
+                        </Col>
+
+                        {/* Admission Year */}
+                        <Col>
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Year</Form.Label>
+                                <Form.Control disabled
+                                    type="text" placeholder='Type your Admission Year'
+                                    name='admissionYear' value={formik.values.admissionYear} onChange={formik.handleChange} />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            {/* Admission Fee */}
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Fee</Form.Label>
+                                <Form.Control
+                                    type="number" placeholder='Type your Admission Fee'
+                                    name='admissionFee' value={formik.values.admissionFee} onChange={formik.handleChange} />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            {/* Admission Source */}
+                            <Form.Group className='mt-3'>
+                                <Form.Label className='mb-0'>Source</Form.Label>
+                                <select
+                                    name="admissionSource"
+                                    className="form-select"
+                                    value={formik.values.admissionSource}
+                                    onChange={formik.handleChange}>
+                                     <option value="">Select Admission Source</option>    
+                                    <option value={"Social"}>Social</option>
+                                    <option value={"Referral"}>Referral</option>
+                                    <option value={"Direct"}>Direct</option>
+                                </select>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className='d-flex gap-3'>
+                    {/* ADD BUTTON */}
+                    <Button type="submit" style={{ backgroundColor: "#4e73df" }}>Add Admission</Button>
+
+                    {/* CLOSE BUTTON*/}
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    </div>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    )
 }
 
-results.forEach(result => console.log(result));
+export default ModalAddAdmission
