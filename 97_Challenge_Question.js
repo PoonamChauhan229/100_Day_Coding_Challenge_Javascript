2a88a812c3c065bcb91a17be2e2e4ab27c329dba
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-01-26 11:24:34 +0530

    98 challenge

--- 97_Challenge_Question.js
+++ 97_Challenge_Question.js
@@ -2,59 +2,58 @@
 
 // class >>Student
 // property marks
-// method grade() >> to ensure the correct grading
+// method grade() to ensure the correct grading. 
 
-// Approach:
+// Approach
 // class Student{}
-// constructor(marks){
-    // marks ? condition >>0-100
-    // if(marks<0 || marks >100) >>print "Invalid Marks ... Enter Bet 0-100"
+// constructor(marks){ 
+    // validation >>Marks >>0-100 >>Print >>Invalid Marks ..Bet 0-100
+    // if(marks<0 ||marks>100){
+        // Invalid Marks ..Bet 0-100
+    // }
     // this.marks=marks
 // }
 
-// grade() 
-// A+  >90 >>Excellent              this.marks>90 >>if
-// A   >80 >>Great Job              this.marks>80 >>else if
-// B   >70 >>Good                   this.marks>70 >>else if
-// C   >60 >>Fair                   this.marks>60 >>else if
-// D   >50 >>Needs Improvement      this.marks>50 >>else if
-// Fail >>Fail >>Below Expectations   Else Part   >>else      
-// if else if else
+//  method >> grade() 
+// A+    >90 >>Excellent                   this.marks>90>>if                      
+// A     >80 >>Great Job                   this.marks>80>>else if
+// B     >70 >>Good                        this.marks>70>>else if
+// C     >60 >>Fair                        this.marks>60>>else if
+// D     >50 >>Needs Improvement           this.marks>50>>else if
+// Fail  >>Fail >>Below Expectations       else part    >>else
+// if else if else 
 
-// Create an instance
+// Create an Instance >>new keyword
 // let student1=new Student(95)
-// student1.grade()  >>A+  >90 >>Excellent
+// student1.grade() >> A+    >90 >>Excellent  
 
 // let student2=new Student(40)
-// student2.grade()  >>Fail >>Fail >>Below Expectations 
+// student2.grade() >>>>Fail >>Below Expectations 
 
-// let student3=new Student(110) >>Invalid Marks ... Enter Bet 0-100
-
+// let student3=new Student(110) >>Invalid Marks ..Bet 0-100
 
 class Student{
     constructor(marks){
-        if(marks<0 || marks>100){
-            console.log("Invalid Marks... Marks should be between 0 to 100")
+        if(marks<0 ||marks>100){
+            console.log("Invalid Marks...Marks should be between 0-100")
         }
         this.marks=marks
     }
-
    grade(){
         if(this.marks>90){
-            console.log("A+- Excellent")
+            console.log("A+ >>Excellent")
         }else if(this.marks>80){
-            console.log("A- Great Job")
+            console.log("A >>Great Job")
         }else if(this.marks>70){
-            console.log("B- Good")
+            console.log("B >>Good")
         }else if(this.marks>60){
-            console.log("C-Fair")
+            console.log("C >>Fair")
         }else if(this.marks>50){
-            console.log("D-Needs Improvement")
+            console.log("D >>Needs Improvement")
         }else{
             console.log("Fail >>Below Expectations")
-        }
+        }           
    }
-    
 }
 
 let student1=new Student(95)
@@ -64,6 +63,9 @@ let student2=new Student(40)
 student2.grade()
 
 let student3=new Student(110)
+
+
+
 
 
 
--- 98_Challenge_Question.js
+++ 98_Challenge_Question.js
@@ -1,1 +1,72 @@
+// Write a BankAccount class with a balance property and methods deposit and withdraw to handle bank transactions
 
+// deposit method: Adds a specified amount to the balance. The amount must be greater than 0.
+// withdraw method: Deducts a specified amount from the balance. The amount must be less than or equal to the current balance.
+
+// Approach:
+//Defines a class named BankAccount. This class will be used to create bank account objects, each with a balance and methods for deposit and withdrawal.
+// class BankAccount {}
+
+// constructor(balance) {
+//     The constructor method initializes a new BankAccount object with an initial balance property. This method is called when an instance is created
+//     this.balance = balance; This will store the balance of the bank account.
+// }
+
+// deposit(amount) {
+//     This method is used to deposit money into the bank account. It checks if the amount is greater than 0 before adding it to the balance.
+//     if (amount > 0) {
+//         this.balance += amount;
+//         console.log(`$${amount} deposited successfully. New balance: $${this.balance}`);
+//     } else {
+//         console.log("Deposit amount must be greater than 0.");
+//     }
+// }
+
+// withdraw(amount) {
+//     This method is used to withdraw money from the bank account. It checks if the amount is greater than 0 and less than or equal to the current balance.
+//     if (amount > 0 && amount <= this.balance) {
+//         this.balance -= amount;
+//         console.log(`$${amount} withdrawn successfully. New balance: $${this.balance}`);
+//     } else {
+//         console.log("Invalid withdrawal amount or insufficient balance.");
+//     }
+// }
+
+// Create an insatance >> new keyword
+// let account=new BankAccount(100)
+// account.deposit(50);                  // Deposit 50     >>150
+// account.deposit(-10);                 // Invalid deposit
+// account.withdraw(20);                  // Withdraw 20   >>130
+// account.deposit(50);                  // Deposit 50     >>180
+
+   
+
+class BankAccount {
+    constructor(balance) {
+      this.balance = balance;
+    }
+  
+    deposit(amount) {
+      if (amount > 0) {
+        this.balance += amount;
+        console.log(`$${amount} deposited successfully. New balance: $${this.balance}`);
+      } else {
+        console.log("Deposit amount must be greater than 0.");
+      }
+    }
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.balance) {
+          this.balance -= amount;
+          console.log(`$${amount} withdrawn successfully. New balance: $${this.balance}`);
+        } else {
+          console.log("Invalid withdrawal amount or insufficient balance.");
+        }
+      }
+    }
+  
+  // Example usage:
+  let account = new BankAccount(100); // Initial balance of 100
+  account.deposit(50);                  // Deposit 50  
+  account.deposit(-10);                 // Invalid deposit
+  account.withdraw(20);                  // Withdraw 20
+  account.deposit(50);                  // Deposit 50
